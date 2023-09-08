464936c857e66ae1618c00278978716ad69508a1
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-08 20:58:58 +0530

    append push pop problem

---
+++ append_push_pop.py
@@ -0,0 +1,15 @@
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+stack_top = stack[-1]
+
+print(stack)
+print("stack top: ",stack_top)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
