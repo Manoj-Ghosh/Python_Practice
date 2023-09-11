cf95e79213c61fde65c18536ce456285d7c635f7
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-11 11:25:49 +0530

    sales problem

---
+++ sales_problem.py
@@ -0,0 +1,10 @@
+n = int(input())
+if (10-n)>5:
+  print("NUMBER OF CANDIES SOLD: ",n)
+  print("NUMBER OF CANDIES AVAILABLE: ",10-n)
+  
+else:
+  print("INVALID INPUT!!")
+  print("NUMBER OF CANDIES LEFT: 10 ",)
+  
+  
