f34a5c8caac0ea7a4ae4b9a36d9ec6a5669c6d42
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-12 17:32:54 +0530

    operation_pop

---
+++ operation_pop.py
@@ -0,0 +1,17 @@
+q = []
+q.append((1,"manoj"))
+q.append((2,"saroj"))
+q.append((4,"piku"))
+q.append((8,"babu"))
+q.sort()
+print(q)
+print(q[0])
+q.pop()
+print(q)
+
+
+b=len(q)
+
+a = q[2]
+print(a[0])
+print(a[1])
