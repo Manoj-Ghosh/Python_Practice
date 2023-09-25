c676d517e7d5f03fbe33d89069c4cd6729405403
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-25 15:00:31 +0530

    sample_problem40

---
+++ sample_problem40.py
@@ -0,0 +1,18 @@
+def alphapat(n):
+  
+  num = 65
+  
+  for i in range(0,n):
+    
+    for j in range(0,i+1):
+      
+      ch = chr(num)
+      
+      print(ch,end = " ")
+    
+    
+    num += 1 
+    print(" ")
+    
+n=5 
+alphapat(n)
